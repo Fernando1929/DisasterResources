dc95f9482ddd3d234ffd0871ae5802589824cd4b
Alejandro Matos Mercado <alejandro,matos1@upr.edu>
2020-04-25 20:45:58 -0400

    Added payment dao

--- backend/dao/payment.py
+++ backend/dao/payment.py
@@ -1,9 +1,18 @@
+from config.dbconfig import pg_config
+import psycopg2
 class PaymentDAO:
     def _init_(self):
-        super().__init__()
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+                                                            pg_config['user'],
+                                                            pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
 
-    def insert(self, user_id):
-        payment_id = 1
+    def insert(self, customer_id):
+        cursor = self.conn.cursor()
+        query = "insert into payment(customer_id) values (%s) returning payment_id;"
+        cursor.execute(query, (customer_id,))
+        payment_id = cursor.fetchone()[0]
+        self.conn.commit()
         return payment_id
 
     def update(self, payment_id, user_id):
