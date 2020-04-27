3a479e3bf61889ae977996f0ea3423058a62759d
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-04-26 20:24:12 -0400

    Added initial dao implementation.

--- backend/dao/admin.py
+++ backend/dao/admin.py
@@ -1,65 +1,99 @@
+from config.dbconfig import pg_config
+import psycopg2
+
 class AdminDAO:
-
-    def __init__(self):
-        super().__init__()
-        
+    
     #admin = user_id, admin_id, admin_firstname, admin_lastname, admin_date_birth, admin_email, admin_phone
-
+    def __init__(self):
+        connection_url = "dbname=%s user=%s password=%s"% (pg_config['dbname'], pg_config['user'], pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
+        
     def getAllAdmins(self):
-        result = [
-            [1, 1, 'Julio', 'Ramires', '23/01/89', 'jramires@gymail.com', '7879999999'],
-            [2, 2, 'Miranda', 'Torres', '23/12/85', 'mtorres@gymail.com', '9999999999']
-        ]
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin Natural Inner Join user_phone;" 
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
     
     def getAdminById(self, admin_id):
-        result = [2, 2, 'Miranda', 'Torres', '23/12/85', 'mtorres@gymail.com', '9999999999']
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin where admin_id = %s;"
+        cursor.execute(query,(admin_id))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAdminsByFirstname(self, admin_firstname):
-        result = [
-            [1, 1, 'Julio', 'Ramires', '23/01/89', 'jramires@gymail.com', '7879999999']
-        ]
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin where user_firstname = %s;"
+        cursor.execute(query,(admin_firstname))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAdminsByLastname(self, admin_lastname):
-        result = [
-            [1, 1, 'Julio', 'Ramires', '23/01/89', 'jramires@gymail.com', '7879999999']
-        ]
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin where user_lastname = %s;"
+        cursor.execute(query,(admin_lastname))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAdminsByEmail(self, admin_email):
-        result = [
-            [1, 1, 'Julio', 'Ramires', '23/01/89', 'jramires@gymail.com', '7879999999']
-        ]
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin where user_email = %s;"
+        cursor.execute(query,(admin_email))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
-    def getAdminsByFirstnameAndLastname(self, admin_firstname, admin_lastname):
-        result = [
-            [1, 1, 'Julio', 'Ramires', '23/01/89', 'jramires@gymail.com', '7879999999']
-        ]
+    def getAdminsByFirstnameAndLastname(self, admin_firstname, admin_lastname):#verify if is need to change the var admin_name to user_name
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin where user_firstname = %s and user_lastname = %s;"
+        cursor.execute(query,(admin_firstname, admin_lastname))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
     
     def getAdminByPhone(self, admin_phone):
-        result = [
-            [1, 1, 'Julio', 'Ramires', '23/01/89', 'jramires@gymail.com', '7879999999']
-        ]
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin Natural Inner Join user_phone where user_phone = %s;"
+        cursor.execute(query,(admin_phone))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAdminsByDateOfBirth(self, admin_date_birth):
-        result = [
-            [1, 1, 'Julio', 'Ramires', '23/01/89', 'jramires@gymail.com', '7879999999']
-        ]
+        cursor = self.conn.cursor()
+        query = "Select * from user Natural Inner Join admin where user_date_birth = %s;"
+        cursor.execute(query,(admin_date_birth))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def insert(self, user_id):
-        admin_id = 1
+        cursor = self.conn.cursor()
+        query = "insert into admin(user_id) values(%s) returning admin_id;"
+        cursor.execute(query,(user_id))
+        admin_id = cursor.fetchone()[0]
+        self.conn.commit()
         return admin_id
 
-    def update(self, admin_id):
-        admin_id = 1
-        return admin_id
+    def update(self, user_id): #Verify is this method is needed
+        return 0
     
-    def delete(self, admin_id):
-        admin_id = 1
-        return admin_id
+    def delete(self, user_id):
+        cursor = self.conn.cursor()
+        query = "delete from resource where user_id = %s;"
+        cursor.execute(query,(user_id))
+        self.conn.commit()
+        return user_id
