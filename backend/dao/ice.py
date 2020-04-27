81928a4c00e296a36d780982ca062cab0961f164
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-04-26 20:16:13 -0400

    Added implementation of the dao and some changes to the handler.

--- backend/dao/ice.py
+++ backend/dao/ice.py
@@ -3,24 +3,18 @@ import psycopg2
 
 class IceDAO:
 
-    def __init__(self):
-        super().__init__()
-        
     #ice = ice_id, resource_id, supplier_id, ice_name, ice_brand, ice_quantity, ice_price , ice_weight
+    def __init__(self):
+        connection_url = "dbname=%s user=%s password=%s"% (pg_config['dbname'], pg_config['user'], pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
 
     def getAllIce(self):
-        result = [
-            [1, 2, 1, "ice", 'ice', 'el angel', 10, 2.50, 5],
-            [2, 1, 2, "ice", 'ice', 'submarine', 8, 2.50, 6]
-        ]
-        return result
-
-
-    def getAllAvailableIce(self):
-        result = [
-            [1, 2, 1, "ice", 'ice', 'el angel', 10, 2.50, 5],
-            [2, 1, 2, "ice", 'ice', 'submarine', 8, 2.50, 6]
-        ]
+        cursor = self.conn.cursor()
+        query = "Select * from resource Natural Inner Join ice;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAllAvailableIce(self):
@@ -29,7 +23,7 @@ class IceDAO:
             [2, 1, 2, "ice", 'ice', 'submarine', 8, 2.50, 6]
         ]
         return result
-    
+
     def getAllReservedIce(self):
         result = [
             [1, 2, 1, "ice", 'ice', 'el angel', 10, 2.50, 5],
@@ -98,9 +92,13 @@ class IceDAO:
         result = [1, 1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]
         return result
 
-    def insert(self, resource_id, supplier_id, ice_name, ice_brand, ice_quantity, ice_price , ice_weight):
-        resource_id = 1
-        return resource_id
+    def insert(self,resource_id, ice_weight):
+        cursor = self.conn.cursor()
+        ice_query = "insert into ice(resource_id, ice_weight) values(%s,%s) returning ice_id"
+        cursor.execute(ice_query,(resource_id, ice_weight))
+        ice_id = cursor.fetchone()[0]
+        self.conn.commit()
+        return ice_id
 
     def delete(self, resource_id):
         resource_id = 1
