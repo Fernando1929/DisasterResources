62f92ae40533d03bf9ad5a681abc605c167ab17d
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-04-30 08:57:26 -0400

    Added the db credentials.

--- backend/config/dbconfig.py
+++ backend/config/dbconfig.py
@@ -1,13 +1,7 @@
 #Databse configuration information
 
 pg_config = {
-<<<<<<< HEAD
     'user' : 'drlusr',
     'passwd' : 'DRLAMF',
     'dbname' : 'drl'
-=======
-    'user' : 'dbuser',
-    'passwd' : 'dbdisaster',
-    'dbname' : 'DRLDB'
->>>>>>> dh
 }
