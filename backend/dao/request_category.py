a6a4f1b5e848f9e8f4f7d3148662023a518008e3
Alejandro Matos Mercado <alejandro,matos1@upr.edu>
2020-05-02 14:17:20 -0400

    Added resource dict to request dict

--- backend/dao/request_category.py
+++ backend/dao/request_category.py
@@ -1,21 +1,21 @@
 from config.dbconfig import pg_config
 import psycopg2
-class ResourceRequestsDAO:
+class RequestCategoryDAO:
     def _init_(self):
         connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
                                                             pg_config['user'],
                                                             pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
-    def insert(self, request_id, resource_id, request_quantity):
+    def insert(self, request_id, category_id, request_quantity):
         cursor = self.conn.cursor()
-        query = "insert into resource_requests(request_id, resource_id, request_quantity) values (%s, %s, %s);"
-        cursor.execute(query, (request_id, resource_id, request_quantity))
+        query = "insert into request_category(request_id, category_id, request_quantity) values (%s, %s, %s);"
+        cursor.execute(query, (request_id, category_id, request_quantity))
         resource_request_id = cursor.fetchone()[0]
         self.conn.commit()
 
-    def update(self, payment_id, user_id):
-        return payment_id
+    def update(self, request_id, category_id, request_quantity):
+        return None
 
-    def delete(self, payment_id):
-        return payment_id
+    def delete(self, request_id, category_id):
+        return None
--- backend/handler/request.py
+++ backend/handler/request.py
@@ -1,23 +1,22 @@
 from flask import jsonify
 from dao.request import RequestDAO
 from dao.customer import CustomerDAO
-from dao.resource_requests import ResourceRequestsDAO
+from dao.request_category import RequestCategoryDAO
 
 class RequestHandler:
 
-    def build_request_dict(self, row):
-         # Array of json for resources
+    def build_request_dict(self, row, resources):
         result = {}
         result['request_id'] = row[0]
         result['customer_id'] = row[1]
         result['request_title'] = row[2]
-        result['request_date'] = row[4]
+        result['request_date'] = row[3]
+        result['request_description'] = row[4]
         result['request_status'] = row[5]
-        result['request_description'] = row[6]
+        result['resources'] = resources
         return result
 
-    def build_request_attributes(self, request_id, customer_id, request_title, request_date, request_status, request_description):
-         # Array of json for resources
+    def build_request_attributes(self, request_id, customer_id, request_title, request_date, request_description, request_status):
         result = {}
         result['request_id'] = request_id
         result['customer_id'] = customer_id 
@@ -27,13 +26,39 @@ class RequestHandler:
         result['request_description'] = request_description
         return result
 
+    def build_resources_dict(self, row):
+        result = {}
+        result['category_id'] = row[0]
+        result['category_name'] = row[1]
+        result['request_quantity'] = row[2]
+        return result
+
+    def fixDict(self, request_list):
+        result_list = []
+        resources_list = []
+        index = 1
+        for row in request_list:
+            if index < len(request_list) and row[0] == request_list[index][0]:
+                resources_list.append(row[6:])
+            else:
+                resources_list.append(row[6:])
+                result = self.build_request_dict(row[:6], self.createResourceDict(resources_list))
+                result_list.append(result)
+                resources_list.clear()
+            index += 1
+        return result_list
+
+    def createResourceDict(self, resources_list):
+        result_list = []
+        for row in resources_list:
+            result = self.build_resources_dict(row)
+            result_list.append(result)
+        return result_list
+    
     def getAllRequests(self):
         dao = RequestDAO()
         request_list = dao.getAllRequests()
-        result_list = []
-        for row in request_list:
-            result = self.build_request_dict(row)
-            result_list.append(result)
+        result_list = self.fixDict(request_list)
         return jsonify(Requests = result_list)
 
     def getRequestById(self, request_id):
@@ -83,11 +108,12 @@ class RequestHandler:
         request_date = json["request_date"]
         request_description = json["request_description"]
         request_status = "Pending"
+        resources = json["resources"]
 
-        if customer_id and request_title and request_date and request_status and request_description:
+        if customer_id and request_title and request_date and request_status and request_description and resources:
             request_dao = RequestDAO()
             request_id = request_dao.insert(customer_id, request_title, request_date, request_status, request_description)
-            result = self.build_request_attributes(request_id, customer_id, request_title, request_date, request_status, request_description)
+            result = self.build_request_attributes(request_id, customer_id, request_title, request_date, request_status, request_description, resources)
             return jsonify(Request = result), 201
         else:
             return jsonify(Error = "Unexpected attributes in post request"), 400
