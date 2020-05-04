3e3a53e35b442112b958402ce45c82e57d73aaf0
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-05-04 17:00:08 -0400

    Implemented the address dao.

--- backend/dao/address.py
+++ backend/dao/address.py
@@ -1,45 +1,81 @@
 from config.dbconfig import pg_config
 import psycopg2
+
 class AddressDAO:
     
+    # address: address_id, user_id, addressline, city, state_province, country, zipcode
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
-                                                            pg_config['user'],
-                                                            pg_config['passwd'])
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'], pg_config['user'],pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
-    # address: address_id, user_id, addressline, city, state_province, country, zipcode
-
     def getAllAddresses(self):
-        result = [[1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]]
+        cursor = self.conn.cursor()
+        query = "Select * from address;" 
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAddressById(self, address_id):
-        result = [1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]
+        cursor = self.conn.cursor()
+        query = "Select * from address where address_id = %s;"
+        cursor.execute(query,(address_id,))
+        result = cursor.fetchone()
         return result
 
     def getAddressesByCity(self, city):
-        result = [[1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]]
+        cursor = self.conn.cursor()
+        query = "Select * from address where city = %s;" 
+        cursor.execute(query,(city,))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAddressesByCountry(self, country):
-        result = [[1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]]
+        cursor = self.conn.cursor()
+        query = "Select * from address where country = %s;" 
+        cursor.execute(query,(country,))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAddressesByZipcode(self, zipcode):
-        result = [[1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]]
+        cursor = self.conn.cursor()
+        query = "Select * from address where zipcode = %s;" 
+        cursor.execute(query,(zipcode,))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAddressesByStateOrProvince(self, state_province):
-        result = [[1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]]
+        cursor = self.conn.cursor()
+        query = "Select * from address where state_province = %s;" 
+        cursor.execute(query,(state_province,))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAddressesByCityAndCountry(self, city, country):
-        result = [[1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]]
+        cursor = self.conn.cursor()
+        query = "Select * from address where city = %s and country = %s;" 
+        cursor.execute(query,(city,country))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAddressesByUserId(self, user_id):
-        result = [[1,1, "Urb. La Quinta Calle Cartier F1", "Yauco", "N/A", "Puerto Rico", "00698"]]
+        cursor = self.conn.cursor()
+        query = "Select * from address where user_id = %s" 
+        cursor.execute(query,(user_id,))
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def insert(self, user_id, addressline, city, state_province, country, zipcode):
@@ -51,7 +87,15 @@ class AddressDAO:
         return address_id
 
     def update(self, address_id, user_id, addressline, city, state_province, country, zipcode):
+        cursor = self.conn.cursor()
+        query = "update address set user_id = %s, addressline = %s, city = %s, state_province = %s, country = %s, zipcode = %s where address_id = %s;" #verify query
+        cursor.execute(query, (user_id, addressline, city, state_province, country, zipcode, address_id))
+        self.conn.commit()
         return address_id
 
     def delete(self, address_id):
+        cursor = self.conn.cursor()
+        query = "delete form address where address_id = %s ;" #verify query
+        cursor.execute(query, (address_id,))
+        self.conn.commit()
         return address_id
