aed3154dbd26a64dc3c89aa9aad6b7ae117ed9f1
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-05-04 17:25:47 -0400

    Addedmanages implementation.

---
+++ backend/dao/manages.py
@@ -0,0 +1,30 @@
+from config.dbconfig import pg_config
+import psycopg2
+
+class ManagesDAO:
+
+    # admin_id, user_id
+    def __init__(self):
+        connection_url = "dbname=%s user=%s password=%s"% (pg_config['dbname'], pg_config['user'], pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
+
+    def insert(self, admin_id, user_id):
+        cursor = self.conn.cursor()
+        query = "insert into manages(admin_id, user_id) values (%s, %s);"
+        cursor.execute(query, (user_id, admin_id))
+        self.conn.commit()
+        return None #Maybe return the tuple
+
+    def update(self, admin_id, user_id): #Verify implementation Maybe Not necesary (Verify later)
+        cursor = self.conn.cursor()
+        query = "update manages set user_id = %s where admin_id = %s;"
+        cursor.execute(query, (user_id, admin_id))
+        self.conn.commit()
+        return None #Maybe return tuple 
+
+    def delete(self, admin_id, user_id): #Verify implementation
+        cursor = self.conn.cursor()
+        query = "delete from manages where user_id = %s and admin_id = %s;"
+        cursor.execute(query, (user_id, admin_id))
+        self.conn.commit()
+        return None #Maybe return the tuple
