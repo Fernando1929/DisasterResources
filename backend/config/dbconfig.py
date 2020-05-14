e7c896a0c71b5352123e0abf12fae6f58698026e
Maria Vilanova <maria.vilanova@upr.edu>
2020-05-14 01:08:24 -0400

    Config fix

--- backend/config/dbconfig.py
+++ backend/config/dbconfig.py
@@ -1,13 +1,7 @@
 #Databse configuration information
 
-# pg_config = {
-#     'user' : 'dbuser',
-#     'passwd' : 'dbdisaster',
-#     'dbname' : 'DRLDB'
-# }
-
 pg_config = {
     'user' : 'dbuser',
-    'passwd' : 'marynova',
+    'passwd' : 'dbdisaster',
     'dbname' : 'DRLDB'
 }
