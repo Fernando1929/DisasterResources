9f78d4a7278d2790f69381daa6b041854021ecec
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-05-14 01:46:30 -0400

    Fixed queries and update method.

---
+++ backend/dao/admin.py
@@ -0,0 +1,103 @@
+from config.dbconfig import pg_config
+import psycopg2
+
+class AdminDAO:
+    
+    #admin = user_id, admin_id, admin_firstname, admin_lastname, admin_date_birth, admin_email, admin_phone
+    
+    def __init__(self):
+        connection_url = "dbname=%s user=%s password=%s"% (pg_config['dbname'], pg_config['user'], pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
+        
+    def getAllAdmins(self):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from admin Natural Inner Join users Natural Inner Join user_phone;" 
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+    
+    def getAdminById(self, admin_id):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from admin Natural Inner Join users Natural Inner Join user_phone where admin_id = %s;"
+        cursor.execute(query,(admin_id,))
+        result = cursor.fetchone()
+        return result
+
+    def getAdminsByFirstname(self, admin_firstname):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from admin Natural Inner Join users Natural Inner Join user_phone where user_firstname = %s;"
+        cursor.execute(query,(admin_firstname,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getAdminsByLastname(self, admin_lastname):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from admin Natural Inner Join users Natural Inner Join user_phone where user_lastname = %s;"
+        cursor.execute(query,(admin_lastname,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getAdminsByEmail(self, admin_email):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from admin Natural Inner Join users Natural Inner Join user_phone where  user_email = %s;"
+        cursor.execute(query,(admin_email,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getAdminsByFirstnameAndLastname(self, admin_firstname, admin_lastname):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from admin Natural Inner Join users Natural Inner Join user_phone where  user_firstname = %s and user_lastname = %s;"
+        cursor.execute(query,(admin_firstname, admin_lastname))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+    
+    def getAdminByPhone(self, admin_phone):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from users Natural Inner Join admin Natural Inner Join user_phone where user_phone = %s;"
+        cursor.execute(query,(admin_phone,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getAdminsByDateOfBirth(self, admin_date_birth):
+        cursor = self.conn.cursor()
+        query = "Select user_id, admin_id, user_firstname, user_lastname, user_date_birth, user_email, phone_id, user_phone from admin Natural Inner Join users Natural Inner Join user_phone where user_date_birth = %s;"
+        cursor.execute(query,(admin_date_birth,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def insert(self, user_id):
+        cursor = self.conn.cursor()
+        query = "insert into admin(user_id) values(%s) returning admin_id;"
+        cursor.execute(query,(user_id,))
+        admin_id = cursor.fetchone()[0]
+        self.conn.commit()
+        return admin_id
+
+    def update(self, admin_id):
+        cursor = self.conn.cursor()
+        query = "Select user_id from admin where suppliadmin_ider_id = %s;"
+        cursor.execute(query,(admin_id,))
+        user_id = cursor.fetchone()[0]
+        return user_id
+
+    def delete(self, admin_id):
+        cursor = self.conn.cursor()
+        query = "delete from admin where admin_id = %s returning user_id;"
+        cursor.execute(query,(admin_id,))
+        user_id = cursor.fetchone()[0]
+        self.conn.commit()
+        return user_id
---
+++ backend/handler/admin.py
@@ -0,0 +1,126 @@
+from flask import jsonify
+from dao.admin import AdminDAO
+from dao.user import UserDAO
+from dao.userPhone import UserPhoneDAO
+
+class AdminHandler:
+
+    def build_admin_attributes(self, user_id, admin_id, admin_firstname, admin_lastname, admin_date_birth, admin_email, admin_phone_id, admin_phone):
+        result = {}
+        result['admin_id'] = admin_id
+        result['user_id'] = user_id
+        result['admin_firstname'] = admin_firstname
+        result['admin_lastname'] = admin_lastname
+        result['admin_date_birth'] = admin_date_birth
+        result['admin_email'] = admin_email
+        result['admin_phone_id'] = admin_phone_id
+        result['admin_phone'] = admin_phone
+        return result
+
+    def build_admin_dict(self, row):
+        result = {}
+        result['user_id'] = row[0]
+        result['admin_id'] = row[1]
+        result['admin_firstname'] = row[2]
+        result['admin_lastname'] = row[3]
+        result['admin_date_birth'] = row[4]
+        result['admin_email'] = row[5]
+        result['admin_phone_id'] = row[6]
+        result['admin_phone'] = row[7]
+        return result
+
+    def getAllAdmins(self):
+        dao = AdminDAO()
+        result = dao.getAllAdmins()
+        result_list = []
+        for row in result:
+            result = self.build_admin_dict(row)
+            result_list.append(result)
+        return jsonify(Admins = result_list)
+
+    def getAdminById(self, admin_id):
+        dao = AdminDAO()
+        row = dao.getAdminById(admin_id)
+        if not row:
+            return jsonify(Error = "Admin Not Found"), 404
+        else:
+            admin = self.build_admin_dict(row)
+            return jsonify(Admin = admin)
+
+    def searchAdmins(self, args):
+        admin_firstname = args.get("admin_firstname")
+        admin_lastname = args.get("admin_lastname")
+        admin_email = args.get('admin_email')
+        admin_phone = args.get('admin_phone')
+        admin_date_birth = args.get('admin_date_birth')
+        dao = AdminDAO()
+        admin_list = []
+        if (len(args) == 2) and admin_firstname and admin_lastname:
+            admin_list = dao.getAdminsByFirstnameAndLastname(admin_firstname , admin_lastname)
+        elif (len(args) == 1) and admin_firstname:
+            admin_list = dao.getAdminsByFirstname(admin_firstname)
+        elif (len(args) == 1) and admin_lastname:
+            admin_list = dao.getAdminsByLastname(admin_lastname)
+        elif(len(args) == 1) and admin_email:
+            admin_list = dao.getAdminsByEmail(admin_email)
+        elif(len(args) == 1) and admin_phone:
+            admin_list = dao.getAdminsByPhone(admin_phone)
+        elif(len(args) == 1) and admin_date_birth:
+            admin_list = dao.getAdminsByDateOfBirth(admin_date_birth)
+        else:
+            return jsonify(Error = "Malformed query string"), 400
+        result_list = []
+        for row in admin_list:
+            result = self.build_admin_dict(row)
+            result_list.append(result)
+        return jsonify(Admins = result_list)
+
+    def insertAdmin(self, json):
+        admin_firstname = json['admin_firstname']
+        admin_lastname = json['admin_lastname']
+        admin_date_birth = json['admin_date_birth']
+        admin_email = json['admin_email']
+        admin_phone = json['admin_phone']
+
+        if admin_firstname and admin_lastname and admin_date_birth and admin_email and admin_phone:
+            dao_user = UserDAO()
+            user_id = dao_user.insert(admin_firstname, admin_lastname, admin_date_birth, admin_email)
+            dao_phone = UserPhoneDAO()
+            admin_phone_id = dao_phone.insert(user_id, admin_phone)   
+            dao_admin = AdminDAO()
+            admin_id = dao_admin.insert(user_id)
+            result = self.build_admin_attributes(user_id, admin_id, admin_firstname, admin_lastname, admin_date_birth, admin_email, admin_phone_id, admin_phone)
+            return jsonify(Admin = result), 201
+        else:
+            return jsonify(Error = "Unexpected attributes in post request"), 400
+
+    def deleteAdmin(self, admin_id):
+        dao_admin = AdminDAO()
+        if not dao_admin.getAdminById(admin_id):
+            return jsonify(Error = "Admin not found."), 404
+        else:
+            dao_user = UserDAO()
+            user_id = dao_admin.delete(admin_id)
+            dao_user.delete(user_id)
+            return jsonify(DeleteStatus = "OK"), 200
+
+    def updateAdmin(self, admin_id, json):
+        dao_admin = AdminDAO()
+        if not dao_admin.getAdminById(admin_id):
+            return jsonify(Error = "Admin not found."), 404
+        else:
+            admin_firstname = json['admin_firstname']
+            admin_lastname = json['admin_lastname']
+            admin_date_birth = json['admin_date_birth']
+            admin_email = json['admin_email']
+            admin_phone = json['admin_phone']
+            admin_phone_id = json["admin_phone_id"]
+
+            if admin_firstname and admin_lastname and admin_date_birth and admin_email and admin_phone and admin_phone_id:
+                user_id = dao_admin.update(admin_id)
+                dao_user = UserDAO()
+                dao_user.update(user_id, admin_firstname, admin_lastname, admin_date_birth, admin_email, admin_phone)
+                result = self.build_admin_attributes(user_id, admin_id, admin_firstname, admin_lastname, admin_date_birth, admin_email, admin_phone_id, admin_phone)
+                return jsonify(Admin = result), 200
+            else:
+                return jsonify(Error = "Unexpected attributes in update request"), 400
